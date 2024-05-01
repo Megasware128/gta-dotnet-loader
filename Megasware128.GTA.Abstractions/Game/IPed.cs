1c3befc2a058bd4dbd32634612e07bf22f3b4e73
Jordy Hulck <Jordy.Hulck@outlook.com>
2024-05-01 14:59:52 +0200

    refactor: Add new properties to IPed interface and implementations
    Added new properties IsStanding, IsAimingGun, and IsInVehicle to the IPed interface and implemented them in the Ped class. Also added properties WantedLevel and CanStartMission to the IPlayerPed interface and implemented them in the PlayerPed class.

--- Megasware128.GTA.Abstractions/Game/IPed.cs
+++ Megasware128.GTA.Abstractions/Game/IPed.cs
@@ -3,8 +3,13 @@
 public interface IPed
 {
     public float Health { get; set; }
+    public bool IsStanding { get; set; }
+    public bool IsAimingGun { get; set; }
+    public bool IsInVehicle { get; set; }
 }
 
 public interface IPlayerPed : IPed
 {
+    public int WantedLevel { get; set; }
+    public bool CanStartMission { get; }
 }
