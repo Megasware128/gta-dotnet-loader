159fda1dac7116f62861be218762d232d6564863
Jordy Hulck <Jordy.Hulck@outlook.com>
2024-05-01 17:18:16 +0200

    refactor: Add new properties to IPed interface and implementations (#12)
    Added new properties IsStanding, IsAimingGun, and IsInVehicle to the IPed interface and implemented them in the Ped class. Also added properties WantedLevel and CanStartMission to the IPlayerPed interface and implemented them in the PlayerPed class.

--- source/peds.h
+++ source/peds.h
@@ -11,27 +11,69 @@ using namespace Megasware128::GTA::Abstractions::Game;
 
 private ref class Ped : public Entity, public IPed {
 private:
-	CPed* _ped;
+    CPed* _ped;
 public:
-	Ped(CPed* ped) : Entity(ped) {
-		_ped = ped;
-	}
+    Ped(CPed* ped) : Entity(ped) {
+        _ped = ped;
+    }
 
-	virtual property float Health {
-		float get() {
-			return _ped->m_fHealth;
-		}
-		void set(float value) {
-			_ped->m_fHealth = value;
-		}
-	}
+    virtual property float Health {
+        float get() {
+            return _ped->m_fHealth;
+        }
+        void set(float value) {
+            _ped->m_fHealth = value;
+        }
+    }
+
+    virtual property bool IsStanding {
+        bool get() {
+            return _ped->m_nPedFlags.bIsStanding;
+        }
+        void set(bool value) {
+            _ped->m_nPedFlags.bIsStanding = value;
+        }
+    }
+
+    virtual property bool IsAimingGun {
+        bool get() {
+            return _ped->m_nPedFlags.bIsAimingGun;
+        }
+        void set(bool value) {
+            _ped->m_nPedFlags.bIsAimingGun = value;
+        }
+    }
+
+    virtual property bool IsInVehicle {
+        bool get() {
+            return _ped->m_nPedFlags.bInVehicle;
+        }
+        void set(bool value) {
+            _ped->m_nPedFlags.bInVehicle = value;
+        }
+    }
 };
 
 private ref class PlayerPed : public Ped, public IPlayerPed {
 private:
-	CPlayerPed* _ped;
+    CPlayerPed* _ped;
 public:
-	PlayerPed(CPlayerPed* ped) : Ped(ped) {
-		_ped = ped;
-	}
+    PlayerPed(CPlayerPed* ped) : Ped(ped) {
+        _ped = ped;
+    }
+
+    virtual property int WantedLevel {
+        int get() {
+            return _ped->GetWantedLevel();
+        }
+        void set(int value) {
+            _ped->SetWantedLevel(value);
+        }
+    }
+
+    virtual property bool CanStartMission {
+        bool get() {
+            return _ped->CanPlayerStartMission();
+        }
+    }
 };
