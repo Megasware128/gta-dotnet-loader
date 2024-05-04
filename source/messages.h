7d4323dcc0cf858ea78623e1c63bb00894aed55e
Jordy Hulck <Jordy.Hulck@outlook.com>
2024-05-04 16:06:54 +0200

    build: Add d3dx9 and d3dx9d dependencies to vcxproj files
    Add d3dx9 and d3dx9d dependencies to the vcxproj files to resolve
    linking issues with the updated code. Also, include messages.cpp file
    to the project for message handling functionality.

--- source/messages.h
+++ source/messages.h
@@ -1,28 +1,9 @@
-#pragma unmanaged
-#include "CMessages.h"
-
-static void addMessage(char* message, int duration) {
-	CMessages::AddMessageJumpQ(message, duration, 0, false);
-}
-
+#pragma once
 #pragma managed
 
-using namespace System;
-using namespace System::Composition;
-using namespace System::Runtime::InteropServices;
-using namespace Megasware128::GTA::Abstractions::Game;
-
-[Export(IMessages::typeid)]
-public ref class Messages : public IMessages {
+[System::Composition::Export(Megasware128::GTA::Abstractions::Game::IMessages::typeid)]
+public ref class Messages : public Megasware128::GTA::Abstractions::Game::IMessages {
 public:
-	virtual void Show(String^ message) {
-		Show(message, 5000);
-	}
-
-	virtual void Show(String^ message, int duration) {
-		IntPtr ptr = Marshal::StringToHGlobalAnsi(message);
-		char* str = static_cast<char*>(ptr.ToPointer());
-		addMessage(str, duration);
-		Marshal::FreeHGlobal(ptr);
-	}
+	virtual void Show(System::String^ message);
+	virtual void Show(System::String^ message, int duration);
 };
