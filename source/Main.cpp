b96010d3cc7eda3982aa53562f0942a922ef4a9f
Jordy Hulck <Jordy.Hulck@outlook.com>
2024-05-17 22:54:08 +0200

    fix: Remove unnecessary `static` keyword from DllMain function.

--- source/Main.cpp
+++ source/Main.cpp
@@ -30,7 +30,7 @@ public:
 
 DotNetLoader* DotNetLoaderPlugin;
 
-static BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
 	switch (ul_reason_for_call)
 	{
