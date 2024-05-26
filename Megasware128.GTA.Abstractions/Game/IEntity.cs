7474d561ee05fc3b91d5b6bffc840b7b3891ef93
Jordy Hulck <Jordy.Hulck@outlook.com>
2024-05-26 03:48:47 +0200

    refactor: Remove unnecessary 'public' access modifier from interfaces.

--- Megasware128.GTA.Abstractions/Game/IEntity.cs
+++ Megasware128.GTA.Abstractions/Game/IEntity.cs
@@ -4,6 +4,6 @@ namespace Megasware128.GTA.Abstractions.Game;
 
 public interface IEntity
 {
-    public Vector3 Position { get; set; }
-    public Vector3 Rotation { get; set; }
+    Vector3 Position { get; set; }
+    Vector3 Rotation { get; set; }
 }
